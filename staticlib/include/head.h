12074dd803630ad72679f9b980e78a269ba35ce7
suifumin <sfmzqbx@sina.com>
2020-11-17 13:40:45 +0800

    静态库制作

---
+++ staticlib/include/head.h
@@ -0,0 +1,7 @@
+#ifndef __HEAD_H_
+#define __HEAD_H_
+int add(int ,int);
+int sub(int ,int);
+int mul(int ,int);
+int div(int ,int);
+#endif
---
+++ staticlib/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "head.h"
+int main(void) {
+    int sum = add (2,24);
+    printf("sum = %d\n",sum);
+    return 0;
+}
